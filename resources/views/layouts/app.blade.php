c15b53d9d520e0ee6a38b75b6976c082f757a41c
AlviusDens <alvius.jdk3@gmail.com>
2026-03-10 14:29:15 +0700

    update fitur login dan dashboard

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="ıd">
+<html lang="id">
 
 <head>
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -9,10 +9,10 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin 2 - Login</title>
+    <title>LATIHAN APP | {{ $title ?? 'Halaman' }}</title>
 
     <!-- Custom fonts for this template-->
-   <link href="{{ asset('sbadmin2/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
+    <link href="{{ asset('sbadmin2/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet" type="text/css">
     <link
         href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
         rel="stylesheet">
@@ -36,7 +36,7 @@
                     <div class="card-body p-0">
                         <!-- Nested Row within Card Body -->
                         <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                            <div class="col-lg-3 d-none d-lg-block bg-login-image"></div>
                             <div class="col-lg-6">
                                 <div class="p-5">
                                     <div class="text-center">
@@ -59,7 +59,7 @@
                                                     Me</label>
                                             </div>
                                         </div>
-                                        <a href="index.html" class="btn btn-primary btn-user btn-block">
+                                        <a href="{{ url('/dashboard') }}" class="btn btn-primary btn-user btn-block">
                                             Login
                                         </a>
                                         <hr>
